4079d1199b3a1859fc6931c6701312f0c2f51466
David Gomez <david.gomez@apiumhub.com>
2018-06-21 18:14:35 +0200

    improve test

--- src/test/kotlin/acceptance/PrintStatementFeature.kt
+++ src/test/kotlin/acceptance/PrintStatementFeature.kt
@@ -14,15 +14,31 @@ import org.mockito.Mockito.verify
 @RunWith(MockitoJUnitRunner::class)
 class PrintStatementFeature {
 
+    @Mock
+    private lateinit var console: Console
+
+    private lateinit var account: Account
+
+    @Before
+    fun initialise(){
+        account = Account()
+    }
 
     @Test
     fun should_print_statement_containing_all_transactions() {
 
+        account.deposit(1000)
+        account.withdraw(100)
+        account.deposit(500)
 
-        verify(console).println("DATE       | AMOUNT  | BALANCE")
-        verify(console).println("10/04/2014 | 500.00  | 1400.00")
-        verify(console).println("02/04/2014 | -100.00 | 900.00")
-        verify(console).println("01/04/2014 | 1000.00 | 1000.00")
+
+        account.printStatement()
+
+        val inOrder = inOrder(console)
+        inOrder.verify(console).println("DATE       | AMOUNT  | BALANCE")
+        inOrder.verify(console).println("10/04/2014 | 500.00  | 1400.00")
+        inOrder.verify(console).println("02/04/2014 | -100.00 | 900.00")
+        inOrder.verify(console).println("01/04/2014 | 1000.00 | 1000.00")
     }
 
 }
